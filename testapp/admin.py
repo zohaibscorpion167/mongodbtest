b0c247d661933bd39a012f4ea4157125b51c8bd2
zohaib167 <zohaibscorpion167@gmail.com>
2021-02-20 17:11:40 +0500

    add more models

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import newmodeltest
+from .models import Contact, Career
+from django.contrib.auth.models import Group
+from import_export.admin import ImportExportModelAdmin
+
+
+admin.site.unregister(Group)
+
 # Register your models here.
 
-admin.site.register(newmodeltest)
+
+@admin.register(Contact)
+class ContactDisplay(ImportExportModelAdmin,admin.ModelAdmin):
+   list_display = ['name', 'email']
+   search_fields = ['name', 'email']
+   list_filter = ['name','email']
+   pass
+
+@admin.register(Career)
+class CareerDisplay(ImportExportModelAdmin,admin.ModelAdmin):
+   list_display = ['firstName', 'email', 'selected','date_applied']
+   search_fields = ['firstName', 'email']
+   list_filter = ['firstName','email','experience']
+   pass
--- testapp/models.py
+++ testapp/models.py
@@ -2,9 +2,30 @@ from django.db import models
 
 # Create your models here.
 
-class newmodeltest(models.Model):
-    name = models.CharField(max_length=200, blank=True)
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=124, null=False, blank=False)
+    email = models.CharField(max_length=124, null=False, blank=False)
+    subject = models.CharField(max_length=12, null=True, blank=True)
+    message = models.TextField(null=False, blank=False)
+    date = models.DateField(auto_now_add=True , null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
-def __str__(self):
-    return self.name
+class Career(models.Model):
+    selected = models.CharField(max_length=124, null=False, blank=False, verbose_name='Position')
+    firstName = models.CharField(max_length=124, null=False, blank=False)
+    lastName = models.CharField(max_length=124, null=False, blank=False)
+    email = models.CharField(max_length=124, null=False, blank=False)
+    phone = models.CharField(max_length=15, null=False, blank=False)
+    employment = models.CharField(max_length=124, null=False, blank=False)
+    experience = models.CharField(max_length=124, null=False, blank=False)
+    date_applied = models.DateField(auto_now_add=True,null=True, blank=True )
+    letter = models.TextField(null=False, blank=False)
+    file = models.FileField(upload_to='documents', max_length=100, blank=True)
+
+
+    def __str__(self):
+        return self.firstName
